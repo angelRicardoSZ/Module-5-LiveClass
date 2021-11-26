abdb376d45f1a985fa23e8f91b3b90ed02d8b460
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2021-11-25 21:38:03 -0600

    merge 3

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,18 +1,12 @@
 const path = require("path");
  
 
-<<<<<<< HEAD
-mainController = {
-​	home: (req,res) => {
-    res.sendFile(path.join(__dirname, "../views/home.html"));
-=======
 const mainController = {
 ​	home: (req, res) => {
         res.sendFile(path.join(__dirname, "../views/home.html"));
    }, 
        about: (req, res) => {
         res.sendFile(path.join(__dirname, "../views/about.html"));
->>>>>>> e997e51fd2d26cb85165613fab5e525ee7a4048d
    }
 };
 
