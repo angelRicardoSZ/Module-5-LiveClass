b85c390d6c29adbe5aaf92ab406142e7c3f16471
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2021-11-29 20:06:45 -0600

    Vistas parciales

--- Class-2/PimientySal/controllers/mainController.js
+++ Class-2/PimientySal/controllers/mainController.js
@@ -3,7 +3,7 @@ const path = require("path");
 /* GET home page. */
 const controller = {
     index:(req,res) => {
-        return res.render(path.join(__dirname,"../views/index.ejs"));
+        return res.render(path.join(__dirname,"../views/index"));
     },
 };
 
