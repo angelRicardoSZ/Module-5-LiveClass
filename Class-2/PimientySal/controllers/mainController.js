664bf58fa0cf63b71851f6854f8a3c4f08ef3665
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2021-11-29 19:57:46 -0600

    Se agrego el archivo de mainController para el index.js

---
+++ Class-2/PimientySal/controllers/mainController.js
@@ -0,0 +1,15 @@
+const path = require("path");
+ 
+/* GET home page. */
+const controller = {
+    index:(req,res) => {
+        return res.render(path.join(__dirname,"../views/index.ejs"));
+    },
+};
+
+module.exports =controller;
+
+
+//router.get('/', function(req, res, next) {
+//    res.render('index', { title: 'Express' });
+//  });
